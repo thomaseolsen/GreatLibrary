efa67cddfb8c1aaf271bf718e219f3b18c2b2cc9
Thomas Erik Olsen <thomas.olsen@tylertech.com>
2019-01-01 09:28:17 -0500

    Started card implementation for book listing

--- TheGreatLibrary/Pages/Contact.cshtml.cs
+++ TheGreatLibrary/Pages/Contact.cshtml.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheGreatLibraryController.Models;
 
 namespace TheGreatLibrary.Pages
 {
     public class ContactModel : PageModel
     {
         public string Message { get; set; }
+        public List<Book> Books { get; set; }
 
         public void OnGet()
         {
             Message = "Your contact page.";
+            Books = new List<Book>();
+            var book1 = new Book();
+            var book2 = new Book();
+            var book3 = new Book();
+            book1.Title = "War and Peace";
+            book1.Author = "Leo Tolstoy";
+            book1.Description = "This is a test description.";
+            Books.Add(book1);
+            book2.Title = "The Double";
+            book2.Author = "Fyodor Dostoyevsky";
+            book2.Description = "This is a test description.";
+            Books.Add(book2);
+            book3.Title = "Crime and Punishment";
+            book3.Author = "Fyodor Dostoyevsky";
+            book3.Description = "This is a test description.";
+            Books.Add(book3);
+            Books.Sort();
         }
     }
 }
---
+++ TheGreatLibraryController/Models/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheGreatLibraryController.Models
+{
+    public class Book : IComparable
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Book otherBook = obj as Book;
+            if (otherBook != null)
+                return this.Title.CompareTo(otherBook.Title);
+            else
+                throw new ArgumentException("Object is not a Book.");
+        }
+    }
+}
