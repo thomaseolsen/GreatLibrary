df447b0f8810e92769600d48c306d3e9755c2193
Thomas Erik Olsen <thomas.olsen@tylertech.com>
2018-12-31 20:08:27 -0500

    Initial Commit

---
+++ TheGreatLibraryController/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TheGreatLibraryController
+{
+    public class Class1
+    {
+    }
+}
---
+++ TheGreatLibraryTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TheGreatLibraryTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
